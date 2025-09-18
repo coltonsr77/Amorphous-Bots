3d3a41d4ba9c316e4d4b696e8560eacfeb3dac28
coltonsr77 <coltonr3@icloud.com>
2025-09-18 18:49:37 -0400

    Update Amorphous.py
    IDK  A Fix

--- Amorphous.py
+++ Amorphous.py
@@ -16,9 +16,7 @@ import unicodedata # Added for Unicode normalization to enhance security
 
 from time import sleep
 import os, os.path
-from keep_alive import keep_alive
 
-keep_alive()
 
 
 def update_watcher():
